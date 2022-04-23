c5a6a441e2860e59ae0060e11a63bfa3d6457e2c
Kumassy <kumassyii@gmail.com>
2022-04-23 17:50:46 +0900

    autofill cd command

--- src/common.ts
+++ src/common.ts
@@ -1,1 +1,14 @@
 export type GameId = 'custom' | 'http' | 'minecraft' | 'factorio'
+
+export function toLocalPort(game: GameId): number {
+  switch(game) {
+    case 'custom':
+      return 3010
+    case 'http':
+      return 80
+    case 'minecraft':
+      return 25565
+    case 'factorio':
+      return 34197
+  }
+}

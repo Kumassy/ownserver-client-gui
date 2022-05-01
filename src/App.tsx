c0d8fbee17eebfe99be3acf63b7df6cf7a9358e4
Kumassy <kumassyii@gmail.com>
2022-05-01 14:39:58 +0900

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Stepper from '@mui/material/Stepper';

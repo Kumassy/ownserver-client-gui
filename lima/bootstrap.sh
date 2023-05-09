886236966e6285e2873c3de2b6da67a45fc4ed07
Kumassy <kumassyii@gmail.com>
2023-05-10 00:11:19 +0900

    add dependencies

--- lima/bootstrap.sh
+++ lima/bootstrap.sh
@@ -28,3 +28,24 @@ source "$HOME/.cargo/env"
 
 # Install Tauri CLI
 cargo install tauri-driver
+
+# Install Docker
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# Game Specific Dependencies
+sudo apt-get install -y \
+    openjdk-19-jre-headless \
+    iperf3
